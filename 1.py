0bae2eb3355a0b525d9c2c884c81588a16925c55
rachidasen <rachidasen@users.noreply.github.com>
2016-04-10 11:50:18 +0530

    Update 1.py
    Old content of code was uploaded, content of code now proper

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 from threading import Thread
 from random import randint
 import time
-import cPickle as pickle
+import pickle
 import numpy as np
 import os
 import sys
 import glob
 import cv2
 import random
+import bob.io.base
  
 bigdataset={}
 
@@ -18,28 +19,28 @@ def load_Imagedata(imagedir='frames', minNumSamplesPerClass=100, imsize=(200,200
 # get the parent directory where this program is stored
   pkl=open('training.pkl','rb')
   total=pickle.load(pkl)
-  print len(total)
-  dist=len(total)/21
+  print (len(total))
+  dist=len(total)/ 21
   # for i in range()
   # bigdataset={}
 
 
   j=-1
   
-  for i in xrange(len(total)):
+  for i in range(len(total)):
       if((i)%dist==0):
           j+=1
           bigdataset["dataset"+str(j)]={}
           # j+=1
       bigdataset["dataset"+str(j)][total[i][0]]=total[i][1]
-  for j in range(11):
-      print "len %d"%len(bigdataset['dataset'+str(j)].keys()),"Values %d"%len(bigdataset['dataset'+str(j)].values())
+  for j in range(21):
+      print ("len %d"%len(bigdataset['dataset'+str(j)].keys()),"Values %d"%len(bigdataset['dataset'+str(j)].values()))
 
 
-  print "Total no of files %d"%len(total)
-  print bigdataset.keys()
-  print len(bigdataset['dataset1'].keys()),"Values %d"%len(bigdataset['dataset1'].values())
-  print "bigdatast keys",
+  print ("Total no of files %d"%len(total))
+  print (bigdataset.keys())
+  print (len(bigdataset['dataset1'].keys()),"Values %d"%len(bigdataset['dataset1'].values()))
+  print ("bigdatast keys")
   # for keys in bigdataset:
   #   print keys
   #   print bigdataset[keys]
@@ -53,24 +54,24 @@ def load_Imagedata(imagedir='frames', minNumSamplesPerClass=100, imsize=(200,200
   myThreadOb1 = MyThread(0)
   myThreadOb1.setName('Thread 1')
  
-  myThreadOb2 = MyThread(7)
-  myThreadOb2.setName('Thread 2')
+  # myThreadOb2 = MyThread(7)
+  # myThreadOb2.setName('Thread 2')
  
-  myThreadOb3 = MyThread(14)
-  myThreadOb3.setName('Thread 3')
+  # myThreadOb3 = MyThread(14)
+  # myThreadOb3.setName('Thread 3')
  
   
  
   # Start running the threads!
   myThreadOb1.start()
-  myThreadOb2.start()
-  myThreadOb3.start()
+  # myThreadOb2.start()
+  # myThreadOb3.start()
   
  
   # Wait for the threads to finish...
-  myThreadOb1.join()
-  myThreadOb2.join()
-  myThreadOb3.join()
+  # myThreadOb1.join()
+  # myThreadOb2.join()
+  # myThreadOb3.join()
   
   print('Main Terminating...')
  
@@ -93,15 +94,15 @@ class MyThread(Thread):
  
   def run(self):
     
-    for loop in range(7):
+    for loop in range(21):
       key='dataset'+str(self.key)
       if isinstance(self.elem,dict):
         sd=0
         for img,i in self.elem.iteritems():
             # print img,i
           im=cv2.imread(img)
-          im=cv2.resize(im,(150,150),interpolation = cv2.INTER_AREA)
-          print "dataset %d curent %d total %d"%(self.key,sd,len(self.elem.keys()))            
+          im=cv2.resize(im,(80,80),interpolation = cv2.INTER_AREA)
+          print ("dataset %d curent %d total %d"%(self.key,sd,len(self.elem.keys())))           
           rs=random.random()
           if not 'X_train' in locals():
                   X_train=im[None,...]
@@ -128,14 +129,14 @@ class MyThread(Thread):
         # X_train /= X_train.std()
  
         # batch={'X_train':X_train,'targets_train':targets_train,'X_val':X_val,'targets_val':targets_val,'X_test':X_test,'targets_test':targets_test}
-        pkl=open('X_train'+key+'.pkl','wb')
-        pickle.dump(X_train,pkl,pickle.HIGHEST_PROTOCOL)
-        pkl.close()
+        f = bob.io.base.HDF5File('X_train'+key, 'w')
+        f.set('image',X_train)
+        del f
  
-        pkl=open('Y_train'+key+'.pkl','wb')
-        pickle.dump(targets_train,pkl,pickle.HIGHEST_PROTOCOL)
-        pkl.close()
-        print "1 file written"
+        pkl2=open('Y_train'+key+'.pkl','wb')
+        pickle.dump(targets_train,pkl2,pickle.HIGHEST_PROTOCOL)
+        pkl2.close()
+        print ("1 file written")
         del targets_train
    
         del X_train        
